fd8aaf0db96cc483147085409fae7849ea612af3
poorvarathore05 <poorvarathore05@gmail.com>
2021-07-08 15:50:42 -0700

    login/signup form

---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import validators
+from wtforms.fields.simple import PasswordField
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField('Username', validators=[validators.InputRequired()])
+
+    password = PasswordField('Password', validators=[
+                             validators.InputRequired()])
+
+
+class SignUpForm(FlaskForm):
+
+    username = StringField('Username', validators=[validators.InputRequired()])
+
+    first_name = StringField('First Name', validators=[
+                             validators.InputRequired()])
+
+    last_name = StringField('Last Name', validators=[
+                            validators.InputRequired()])
+
+    password = PasswordField('Password', validators=[
+                             validators.InputRequired()])
+
+    email = StringField('Email', validators=[validators.InputRequired()])
