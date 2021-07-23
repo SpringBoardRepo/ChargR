30e86bdb4530b0d7fe5969dc6f55ebc9b1e3bc2a
poorvarathore05 <poorvarathore05@gmail.com>
2021-07-22 17:58:11 -0700

    change in font size

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ app.config["MAP_BOX_API_KEY"] = os.environ.get(
     'MAP_BOX_API_KEY', MAP_BOX_API_KEY)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL').replace(
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    'DATABASE_URL').replace(
         "postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SQLALCHEMY_ECHO'] = True
